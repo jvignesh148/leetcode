4919a515bf54c215e2fcbe5ca0b6385e6c58df83
VIGNESH J <133660156+jvignesh148@users.noreply.github.com>
2025-02-28 12:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (64.23%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=matrix.length-1;i>0;i--){
+            for(int j=matrix[i].length-1;j>0;j--){
+                if(matrix[i][j]!=matrix[i-1][j-1]) return false;
+            }
+        }
+        return true;
+    }
+}
