c833158f548d299102d3ad30bf832c7a9c01f094
VIGNESH J <jvignesh148@gmail.com>
2025-05-06 07:06:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,11 +1,5 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] arr=new int[nums.length];
-        int ind=0;
-        for(int i=0;i<nums.length;i++){
-            arr[ind]=nums[nums[i]];
-            ind+=1;
-        }
-        return arr;
+        
     }
 }
