e2094586f8a62678bf1c6592d442e16e39343e04
VIGNESH J <jvignesh148@gmail.com>
2025-08-07 14:12:29 +0530

    Time: 3 ms (99.88%), Space: 41.8 MB (70.59%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    static{
+        for(int i=0;i<10000;i++) change(0,new int[] {1,1});
+    }
+    public static int change(int amount, int[] coins) {
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        for(int coin : coins){
+            for(int i=coin; i<=amount;i++){
+                dp[i]+=dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
