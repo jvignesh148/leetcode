c022aba43482c733e2ac16e257a0b59b167673ad
VIGNESH J <jvignesh148@gmail.com>
2025-09-22 08:24:56 +0530

    Time: 0 ms (100%), Space: 48.2 MB (21.12%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int low=0,high=citations.length-1;
+        int result=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(citations[mid]==citations.length-mid) return citations.length-mid;
+            else if(citations[mid]<citations.length-mid) low=mid+1;
+            else {
+                result=citations.length-mid;
+                high=mid-1;
+            }
+        }
+        return result;
+    }
+}
