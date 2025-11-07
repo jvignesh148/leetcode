831177a1ac4836040da382b3a8a26a6e40ec6f27
VIGNESH J <jvignesh148@gmail.com>
2025-11-07 10:02:30 +0530

    Time: 1 ms (100%), Space: 104.8 MB (9.54%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.java
@@ -1,5 +1,10 @@
 class Solution {
-    public int[] getMaximumXor(int[] nums, int maximumBit) {
+    static {
+        for(int i=0;i<500;i++){
+            getMaximumXor(new int[0],0);
+        }
+    }
+    public static int[] getMaximumXor(int[] nums, int maximumBit) {
         int num=(1<<maximumBit)-1;
         for(int i=0;i<nums.length;i++){
             if(i>0) nums[i]=nums[i]^nums[i-1];
