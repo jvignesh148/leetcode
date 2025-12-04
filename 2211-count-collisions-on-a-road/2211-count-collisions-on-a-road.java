0528fff4dbb4678d2e7faa54888715e269091fd2
VIGNESH J <jvignesh148@gmail.com>
2025-12-04 12:31:29 +0530

    Time: 126 ms (11.69%), Space: 47.9 MB (7.26%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countCollisions(String directions) {
+        Stack<Character> st = new Stack<>();
+        int result = 0;
+        for (char c : directions.toCharArray()) {
+            while (!st.isEmpty() && isValid(st.peek(), c)) {
+                if (c == 'L' && st.peek() == 'R') {
+                    st.pop();
+                    c = 'S';
+                    result += 2;
+                } else if (c == 'L' && st.peek() == 'S') {
+                    result += 1;
+                    c = 'S';
+                } else if (c == 'S' && st.peek() == 'R') {
+                    result += 1;
+                    st.pop();
+                } else
+                    break;
+            }
+            st.push(c);
+        }
+        return result;
+    }
+
+    public static boolean isValid(char c1, char c2) {
+        return (c1 == 'S' && c2 == 'L') || (c1 == 'R' && (c2 == 'S' || c2 == 'L'));
+    }
+}
